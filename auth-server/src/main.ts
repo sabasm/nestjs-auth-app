e1fe8edc77c6da642b9a44359b70f1895d2a5cb3
Sabas Mendivil <43766808+sabasm@users.noreply.github.com>
2023-09-11 01:28:34 +0000

    feat: hybrid auth working session and jwt

--- auth-server/src/main.ts
+++ auth-server/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import config from '../config';
+import * as session from 'express-session';
+import * as passport from 'passport';
 import helmet from 'helmet'; // Cambio aquí
 
 async function bootstrap() {
@@ -9,11 +11,31 @@ async function bootstrap() {
   // Add basic security policies including CSP
   app.use(helmet());
 
-    app.enableCors({
-      origin: config.CORS_ORIGIN,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    });
+  // Enable CORS
+  app.enableCors({
+    origin: config.CORS_ORIGIN,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // Session config
+  app.use(
+    session({
+      secret: config.JWT_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: config.COOKIE_MAX_AGE,
+        httpOnly: true,
+        secure: config.isSecure,
+        sameSite: 'strict',
+      },
+    }),
+  );
+
+  // Initialize Passport
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   await app.listen(config.PORT);
 }
